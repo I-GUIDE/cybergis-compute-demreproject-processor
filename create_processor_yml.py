0b385761c06a9505246d056587ebc99722b954fb
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2023-09-25 22:33:11 -0500

    Update create_processor_yml.py

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -6,7 +6,7 @@ import os
 demmerge_jobid = os.environ['param_raster_path']
 input_path = f'/compute_scratch/{demmerge_jobid}'
 
-processor_dict = {'$1': {'DEMReproject': {'raster_path':os.environ['param_input_path'],'site_id':os.environ['param_site_id'],'resolution':os.environ['param_resolution']}}}}
+processor_dict = {'$1': {'DEMReproject': {'raster_path':os.environ['param_input_path'],'site_id':os.environ['param_site_id'],'resolution':os.environ['param_resolution']}}}
 
 with open('/job/executable/demreproject.yml','w') as demfile:
     yaml.dump(processor_dict,demfile)
